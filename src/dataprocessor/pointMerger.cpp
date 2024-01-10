8a8fb8ac96dbc9c407bbe8cd7956fe3ea64266ee
Hyungtae Lim <shapelim@kaist.ac.kr>
2024-01-10 20:28:59 +0900

    Add saveFormat as a parameter (bin is preferred)

--- src/dataprocessor/pointMerger.cpp
+++ src/dataprocessor/pointMerger.cpp
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
   std::string              absPath;
   std::vector<std::string> process_lidar_list;
   std::string              saveDir;
-  std::string              saveFormat = "bin";
+  std::string              saveFormat;
   std::string              trajectoryDir;
 
   nh.param<std::string>("/dataprocessor/dataset_root", absPath, "/home/ericlab");
@@ -26,11 +26,12 @@ int main(int argc, char **argv)
                                      {"Ouster", "Velodyne", "Livox", "Aeva"});
   nh.param<std::string>("/dataloader/abs_data_dir", saveDir, "/home/ericlab");
   nh.param<std::string>("/dataprocessor/save_ins_to_LiDAR_root", trajectoryDir, "/home/ericlab");
+  nh.param<std::string>("/dataprocessor/saveFormat", saveFormat, "bin");
 
-  PointCloudProcessor OusterProcessor(absPath, "Ouster", saveDir, trajectoryDir);
-  PointCloudProcessor VelodyneProcessor(absPath, "Velodyne", saveDir, trajectoryDir);
-  PointCloudProcessor AviaProcessor(absPath, "Avia", saveDir, trajectoryDir);
-  PointCloudProcessor AevaProcessor(absPath, "Aeva", saveDir, trajectoryDir);
+  PointCloudProcessor OusterProcessor(absPath, "Ouster", saveDir, trajectoryDir, saveFormat);
+  PointCloudProcessor VelodyneProcessor(absPath, "Velodyne", saveDir, trajectoryDir, saveFormat);
+  PointCloudProcessor AviaProcessor(absPath, "Avia", saveDir, trajectoryDir, saveFormat);
+  PointCloudProcessor AevaProcessor(absPath, "Aeva", saveDir, trajectoryDir, saveFormat);
 
   const auto &ts_o  = OusterProcessor.timestamp_lists_;
   const auto &ts_v  = VelodyneProcessor.timestamp_lists_;
--- src/dataprocessor/pointSaver.cpp
+++ src/dataprocessor/pointSaver.cpp
@@ -14,18 +14,20 @@ int main(int argc, char **argv)
   std::string absPath;
   std::vector<std::string> process_lidar_list;
   std::string saveDir;
+  std::string saveFormat;
   std::string trajectoryDir;
 
   nh.param<std::string>("/dataprocessor/dataset_root", absPath, "/home/ericlab");
   nh.param<std::vector<std::string>>("/dataprocessor/process_lidar_list", process_lidar_list, {"Ouster", "Velodyne", "Livox", "Aeva"});
   nh.param<std::string>("/dataloader/abs_data_dir", saveDir, "/home/ericlab");
   nh.param<std::string>("/dataprocessor/save_ins_to_LiDAR_root", trajectoryDir, "/home/ericlab");
+  nh.param<std::string>("/dataprocessor/saveFormat", saveFormat, "bin");
 
   
   for(auto sensorType : process_lidar_list)
   {
     signal(SIGINT, signal_callback_handler);
-    PointCloudProcessor processor(absPath, sensorType, saveDir, trajectoryDir);
+    PointCloudProcessor processor(absPath, sensorType, saveDir, trajectoryDir, saveFormat);
     processor.ProcessBinFiles();
   }
   
